fdc99a02fb2764f9787e03affdb6860af8156660
Celinda Cueto <71739893+celycueto-ux@users.noreply.github.com>
2020-10-08 16:50:52 -0500

    Add files via upload

---
+++ src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const PrimeraApp = () => {
+
+    return (
+        <>
+           <h1>Hola Mundo en React</h1>
+        </>
+    );
+}
+
+
+export default PrimeraApp;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimeraApp from './PrimeraApp'
+
+ReactDOM.render(<PrimeraApp />, document.getElementById('root'));
